f94e93992f4a34118afda1c20b32ec6b4b248906
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-01-03 20:33:42 +0600

    define the function currying

--- functionCurrying.js
+++ functionCurrying.js
@@ -1,1 +1,17 @@
- 
+// currying function
+
+// const add = (a) => (b) => a + b;
+
+function add(a) {
+  return function (b) {
+    return a + b;
+  };
+}
+console.log(add(5)(90));
+
+const totalPrice = (discount) => (amount) => amount - amount * discount;
+
+const withDiscount = totalPrice(0.3);
+console.log(withDiscount(100));
+console.log(withDiscount(200));
+console.log(withDiscount(300));
